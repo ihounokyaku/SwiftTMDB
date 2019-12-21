1cd6be016995f695aae8055b6b486188ccf485cb
Dylan <kamisama42@gmail.com>
2019-12-21 12:09:19 +0700

    before upgrade to 5.1

---
+++ SwiftTMDB_MacOS/SwiftTMDB_MacOS.h
@@ -0,0 +1,19 @@
+//
+//  SwiftTMDB_MacOS.h
+//  SwiftTMDB_MacOS
+//
+//  Created by Dylan Southard on 2019/07/03.
+//  Copyright © 2019 Dylan Southard. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftTMDB_MacOS.
+FOUNDATION_EXPORT double SwiftTMDB_MacOSVersionNumber;
+
+//! Project version string for SwiftTMDB_MacOS.
+FOUNDATION_EXPORT const unsigned char SwiftTMDB_MacOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftTMDB_MacOS/PublicHeader.h>
+
+
---
+++ SwiftTMDB_iOS/SwiftTMDB_iOS.h
@@ -0,0 +1,19 @@
+//
+//  SwiftTMDB_iOS.h
+//  SwiftTMDB_iOS
+//
+//  Created by Dylan Southard on 2019/07/03.
+//  Copyright © 2019 Dylan Southard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftTMDB_iOS.
+FOUNDATION_EXPORT double SwiftTMDB_iOSVersionNumber;
+
+//! Project version string for SwiftTMDB_iOS.
+FOUNDATION_EXPORT const unsigned char SwiftTMDB_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftTMDB_iOS/PublicHeader.h>
+
+
